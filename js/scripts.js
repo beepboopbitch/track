8719ef861d6de0a3da114e2c139bd0d43cf03330
Nayomi Morita <nayomi.morita@gmail.com>
2019-01-18 10:57:33 -0800

    revert to old save

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
-  $("#print").submit(function(event) {
-    event.preventDefault();
-    var result = $("select#q1").val();
+  $("form#tracks").submit(function(event) {
+    var q1 = $("select#q1").val();
+    var q2 = $("select#q2").val();
+    var q3 = $("select#q3").val();
+    var q4 = $("select#q4").val();
+    var q5 = $("select#q5").val();
 
-    $("#output").text(result);
+    var answer = 100;
+    if (q1 === 'frontend') {
+      answer += 50;
+    }
+
+
+
+    $("#suggest").text(answer);
+    
+
+    event.preventDefault();
   });
 });
